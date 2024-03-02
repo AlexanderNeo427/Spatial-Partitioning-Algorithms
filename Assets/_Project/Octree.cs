f9dc976a06636027ed8817b083f3c3afc773a57e
Alexander Neo <AlexanderNeo427@gmail.com>
2024-03-02 16:35:44 +0800

    Fixed some bug with the bounding box initialization

--- Assets/_Project/Octree.cs
+++ Assets/_Project/Octree.cs
@@ -9,19 +9,17 @@ namespace Neo {
 
     public sealed class Octree {
         const int MAX_OBJECTS_COUNT = 15;
-        const int MAX_DEPTH = 5;
+        const int MAX_DEPTH = 1;
         const int MIN_SIZE = 10;
 
         public Octree[] ChildNodes { get; private set; }
         public Boundary NodeBoundary { get; private set; }
         public bool IsLeafNode { get; private set; }
-        public int CurrentDepth { get; private set; }
- 
+
         public Octree(Boundary boundary, List<GameObject> gameObjects, int currentDepth) {
             ChildNodes = null;
             NodeBoundary = boundary;
             IsLeafNode = true;
-            CurrentDepth = currentDepth;
 
             bool needsToSubdivide = gameObjects.Count > MAX_OBJECTS_COUNT;
             if (!needsToSubdivide) return; 
@@ -35,18 +33,14 @@ namespace Neo {
             // If 'needsToSubdivide' and 'canSubdivide'
             IsLeafNode = false;
             ChildNodes = new Octree[8];
-            Vector3 childSize = new(
-                (boundary.max.x - boundary.min.x) * 0.5f,
-                (boundary.max.y - boundary.min.y) * 0.5f,
-                (boundary.max.z - boundary.min.z) * 0.5f
-            );
+            Vector3 childSize = (boundary.max - boundary.min) * 0.5f;
             int idx = 0;
             for (int x = 0; x < 2; x++) {
-                float startX = boundary.min.x + x * childSize.x;
+                float startX = boundary.min.x + (x * childSize.x);
                 for (int y = 0; y < 2; y++) {
-                    float startY = boundary.min.y + y * childSize.y;
+                    float startY = boundary.min.y + (y * childSize.y);
                     for (int z = 0; z < 2; z++) {
-                        float startZ = boundary.min.z + z * childSize.z;
+                        float startZ = boundary.min.z + (z * childSize.z);
                         ChildNodes[idx++] = new Octree(new Boundary {
                             min = new Vector3(startX, startY, startZ),
                             max = new Vector3(startX + childSize.x, startY + childSize.y, startZ + childSize.z)
@@ -56,7 +50,7 @@ namespace Neo {
             }
         }
 
-        public static void GetLeafNodeBoundaries(Octree node, ref List<Boundary> outLeafNodeBoundaries, ref int itrCount) {
+        public static void GetLeafNodeBoundaries(Octree node, ref List<Boundary> outLeafNodeBoundaries) {
             if (node == null) return; 
             for (int i = 0; i < 8; i++) {
                 Octree childNode = node.ChildNodes[i];
@@ -65,9 +59,8 @@ namespace Neo {
                     outLeafNodeBoundaries.Add(childNode.NodeBoundary);
                 }
                 else {
-                    GetLeafNodeBoundaries(childNode, ref outLeafNodeBoundaries, ref itrCount);
+                    GetLeafNodeBoundaries(childNode, ref outLeafNodeBoundaries);
                 }
-                itrCount++;
             }
         }
     }
--- Assets/_Project/WorldInitializer.cs
+++ Assets/_Project/WorldInitializer.cs
@@ -16,80 +16,83 @@ namespace Neo {
         List<GameObject> m_allSpheres;
         Octree m_rootNode;
 
+        List<Color> m_debugColors = new();
+
         void Awake() {
             Assert.IsNotNull(_spherePrefab);
             GetComponent<MeshRenderer>().enabled = false;
         }
 
         void Start() {
+            for (int i = 0; i < 30; i++) {
+                m_debugColors.Add(new Color(
+                    Random.Range(0f, 1f),
+                    Random.Range(0f, 1f),
+                    Random.Range(0f, 1f)
+                )); 
+            }
+
             m_worldBoundary = _InitializeBoundingBoxWalls(0.2f);
             m_allSpheres = _InitializeSpheres((int)_sphereCount, Random.Range(0.2f, 0.8f), m_worldBoundary);
         }
 
         void Update() {
-            m_rootNode = new Octree(m_worldBoundary, m_allSpheres, 0);
+            //m_rootNode = new Octree(m_worldBoundary, m_allSpheres, 0);
         }
 
         void OnDrawGizmos() {
             if (!Application.isPlaying) { return; }
-            _DrawBoundary(m_worldBoundary);
 
-            int debugItrCount = 0;
-            List<Boundary> leafNodeBoundaries = new();
-            Octree.GetLeafNodeBoundaries(m_rootNode, ref leafNodeBoundaries, ref debugItrCount);
-
-            Debug.Log(debugItrCount);
-
-            int leItr = 0;
-            leafNodeBoundaries.ForEach(boundary => {
-                Debug.Log(leItr++);
-                Debug.Log("Min: " + boundary.min);
-                Debug.Log("Max: " + boundary.max);
-                Debug.Log("");
-                _DrawBoundary(boundary);
-            });
+            //List<Boundary> leafNodeBoundaries = new();
+            //Octree.GetLeafNodeBoundaries(m_rootNode, ref leafNodeBoundaries);
+            //
+            //int colorIdx = 0;
+            //leafNodeBoundaries.ForEach(boundary => {
+            //    _DrawBoundary(boundary, m_debugColors[++colorIdx]);
+            //});
+            //_DrawBoundary(m_worldBoundary, Color.white);
         }
 
         private Boundary _InitializeBoundingBoxWalls(float depth) {
             GameObject front = GameObject.CreatePrimitive(PrimitiveType.Cube);
             front.name = "Front Wall";
-            front.transform.position += transform.forward * ((transform.localScale.z * 0.5f) + (depth * 0.5f));
+            front.transform.localPosition = transform.position + transform.forward * ((transform.localScale.z * 0.5f) + (depth * 0.5f));
             front.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, depth);
             front.GetComponent<MeshRenderer>().enabled = false;
 
             GameObject back = GameObject.CreatePrimitive(PrimitiveType.Cube);
             back.name = "Back Wall";
-            back.transform.position -= transform.forward * ((transform.localScale.z * 0.5f) + (depth * 0.5f));
+            back.transform.localPosition = transform.position - transform.forward * ((transform.localScale.z * 0.5f) + (depth * 0.5f));
             back.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, depth);
             back.GetComponent<MeshRenderer>().enabled = false;
 
             GameObject right = GameObject.CreatePrimitive(PrimitiveType.Cube);
             right.name = "Right Wall";
-            right.transform.position += transform.right * ((transform.localScale.x * 0.5f) + (depth * 0.5f));
+            right.transform.localPosition += transform.position + transform.right * ((transform.localScale.x * 0.5f) + (depth * 0.5f));
             right.transform.localScale = new Vector3(depth, transform.localScale.y, transform.localScale.z);
             right.GetComponent<MeshRenderer>().enabled = false;
 
             GameObject left = GameObject.CreatePrimitive(PrimitiveType.Cube);
             left.name = "Left Wall";
-            left.transform.position -= transform.right * ((transform.localScale.x * 0.5f) + (depth * 0.5f));
+            left.transform.localPosition = transform.position - transform.right * ((transform.localScale.x * 0.5f) + (depth * 0.5f));
             left.transform.localScale = new Vector3(depth, transform.localScale.y, transform.localScale.z);
             left.GetComponent<MeshRenderer>().enabled = false;
 
             GameObject up = GameObject.CreatePrimitive(PrimitiveType.Cube);
             up.name = "Up Wall";
-            up.transform.position += transform.up * ((transform.localScale.y * 0.5f) + (depth * 0.5f));
+            up.transform.localPosition = transform.position + transform.up * ((transform.localScale.y * 0.5f) + (depth * 0.5f));
             up.transform.localScale = new Vector3(transform.localScale.x, depth, transform.localScale.z);
             up.GetComponent<MeshRenderer>().enabled = false;
 
             GameObject down = GameObject.CreatePrimitive(PrimitiveType.Cube);
             down.name = "Down Wall";
-            down.transform.position -= transform.up * ((transform.localScale.y * 0.5f) + (depth * 0.5f));
+            down.transform.localPosition = transform.position - transform.up * ((transform.localScale.y * 0.5f) + (depth * 0.5f));
             down.transform.localScale = new Vector3(transform.localScale.x, depth, transform.localScale.z);
             down.GetComponent<MeshRenderer>().enabled = false;
 
             return new Boundary {
-                min = transform.position - transform.localScale * 0.5f,
-                max = transform.position + transform.localScale * 0.5f
+                min = transform.position - transform.lossyScale * 0.5f,
+                max = transform.position + transform.lossyScale * 0.5f
             };
         }
 
@@ -100,24 +103,26 @@ namespace Neo {
             for (int i = 0; i < sphereCount; i++) {
                 GameObject sphere = Instantiate(_spherePrefab);
                 sphere.transform.localScale = new Vector3(radius, radius, radius);
-                sphere.transform.localPosition = new Vector3(
+                sphere.transform.position = new Vector3(
                     Random.Range(worldBoundary.min.x + PADDING, worldBoundary.max.x - PADDING),
-                    Random.Range(worldBoundary.min.x + PADDING, worldBoundary.max.x - PADDING),
-                    Random.Range(worldBoundary.min.x + PADDING, worldBoundary.max.x - PADDING)
+                    Random.Range(worldBoundary.min.y + PADDING, worldBoundary.max.y - PADDING),
+                    Random.Range(worldBoundary.min.z + PADDING, worldBoundary.max.z - PADDING)
                 );
 
                 sphere.transform.rotation = Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0, 360f), 0f);
                 Rigidbody rb = sphere.GetComponent<Rigidbody>();
-                rb.AddForce(sphere.transform.forward * Random.Range(50f, 500f));
+                rb.AddForce(sphere.transform.forward * Random.Range(20f, 100f));
 
                 allSpheres.Add(sphere);
             }
             return allSpheres;
         }
 
-        void _DrawBoundary(Boundary boundary) {
+        void _DrawBoundary(Boundary boundary, Color color) {
             Vector3 vMinToMax = boundary.max - boundary.min;
             float distanceMinToMax = Vector3.Magnitude(vMinToMax);
+
+            Gizmos.color = color;
             Gizmos.DrawWireCube(boundary.min + vMinToMax.normalized * distanceMinToMax * 0.5f, transform.localScale);
         }
     }
